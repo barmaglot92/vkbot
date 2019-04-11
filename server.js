deea1c9f8367e45366d844799dc1b4fcfa8445f9
Andrey Zhvakin <barmaglot@yandex-team.ru>
2019-04-11 19:01:35 +0300

    init

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const VkBot = require("node-vk-bot-api");
+
+const app = express();
+const bot = new VkBot({
+  token: process.env.TOKEN,
+  confirmation: process.env.CONFIRMATION
+});
+
+bot.on(ctx => {
+  ctx.reply("Hello!");
+});
+
+app.use(bodyParser.json());
+
+app.post("/", bot.webhookCallback);
+
+bot.sendMessage(540314789, 'Hello!', 'photo1_1')
+
+app.listen(process.env.PORT);
